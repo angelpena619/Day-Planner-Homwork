2ee13d7904a809db92b17925341bc4da390bc175
Angel <angelpena619@gmail.com>
2021-04-10 19:19:15 -0700

    adding comments and screen shots

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// created seprate intervals to break apart the values of actual time.
+
 var hour9am = $("#9");
 var hour10am = $("#10");
 var hour11am = $("#11");
@@ -9,9 +11,10 @@ var hour4pm = $("#16");
 var hour5pm = $("#17");
 
 var time = moment();
+// below sets current time using moment.js
 var currentTime = parseInt(moment().format("H"));
 console.log(currentTime)
-
+// worked with tutor to apply a function to check time aka "time checker" the day of time the schedula can be set to, either oast, present or future. indicated by dirrent colors accesing from CSS files. 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 function timeChecker() {
   
@@ -31,7 +34,7 @@ function timeChecker() {
   });
 }
 timeChecker();
-
+// worked with tutor to apply a function to save the user input, if user cicks on "saveBtn" it will initiate a function to do an local storage for user input, for later consumption.
 function savePlanner() {
 
     $(".time-block").each(function () {
@@ -44,7 +47,6 @@ function savePlanner() {
         }
     });
 }
-
 savePlanner();
 var saveBtn = $(".saveBtn");
 
